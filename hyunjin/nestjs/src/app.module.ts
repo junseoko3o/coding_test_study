5ae0079b7f2acadd1dd399690060e749bb4d5455
razberrytaki <14997359+razberrytaki@users.noreply.github.com>
2025-03-23 22:40:06 +0900

    feat: 신규 예시 module에 추가

--- hyunjin/nestjs/src/app.module.ts
+++ hyunjin/nestjs/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleModule } from './sample/sample.module';
 import { GetPostModule } from './get-post/get-post.module';
+import { RequestPayloadModule } from './request-payload/request-payload.module';
 
 @Module({
-  imports: [SampleModule, GetPostModule],
+  imports: [SampleModule, GetPostModule, RequestPayloadModule],
   controllers: [AppController],
   providers: [AppService],
 })
