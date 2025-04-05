a9fd9f22b6929e2bfd7cc816032b99e08981488e
junseoko3o <github.yam117@passinbox.com>
2025-04-06 01:43:18 +0900

    Fix: gitignore

--- junseok/permutation/make_permutation.cpp
+++ junseok/permutation/make_permutation.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> v = {};
+vector<int> v;
 
-void printV(vector<int> &v) {
-  cout << "result";
+void printV(vector<int> &v) { // 원본도 같이 수정 (참조자 &)
+  cout << "result ";
   for(int i = 0; i < v.size(); i++) {
     cout << v[i] << " ";
   }
@@ -14,7 +14,7 @@ void printV(vector<int> &v) {
 void makePermutation(int n, int r, int depth) {
   cout << n << " : " << r << " : " << depth << "\n";
 
-  if (r == depth) {
+  if (r == depth) { // 기저사례
     printV(v);
     return;
   }
@@ -29,5 +29,9 @@ void makePermutation(int n, int r, int depth) {
 int main () {
   for (int i = 1; i < 4; i++) v.push_back(i);
   makePermutation(3, 3, 0);
+  // 굳이 외울필욘 없다 do while permutation이 있으니까
   return 0;
 }
+
+
+// 도식화 해볼것.
