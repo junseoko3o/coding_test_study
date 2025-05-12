ecb73023f5d0bfb60dabf36770ead16f618769e5
razberrytaki <14997359+razberrytaki@users.noreply.github.com>
2025-05-12 16:55:01 +0900

    comment: 주석 추가

--- hyunjin/0 week/01.permutation/permutation_vector.cpp
+++ hyunjin/0 week/01.permutation/permutation_vector.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main () {
-  vector<int> a = { 2, 1, 3};
+  vector<int> a = { 2, 1, 3}; // 요새 C++에선 기본 배열보단 vector를 더 많이 사용 함
 
   sort(a.begin(), a.end()); // 순열은 항상 sort가 되어있어야 함!!
 
@@ -11,5 +11,4 @@ int main () {
   } while (next_permutation(a.begin(), a.end()));
 
   return 0;
-
 }
